42acfa1cdde3fa2b053516f0a2aa10e45b9249bf
Aditya3403 <adityabansal3403@gmail.com>
2025-02-05 16:52:55 +0530

    Fix dependencies

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,8 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600;700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" 
                     rel="stylesheet"
                 />
+                {/* Favicon link */}
+                <link rel="icon" href="/favicon.ico" />
                 {/* You can add any additional metadata or links here */}
             </Head>
             <body>
